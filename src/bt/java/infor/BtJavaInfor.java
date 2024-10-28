f56d826f731ee9cf19fe0470b482d1e1e9abf1a0
doem5 <doem5@MSI>
2024-10-29 01:00:06 +0700

    bt java

---
+++ src/bt/java/infor/BtJavaInfor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bt.java.infor;
+
+/**
+ *
+ * @author doem5
+ */
+public class BtJavaInfor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Le Hai Dang");
+        System.out.println("CMU TMP15");
+        System.out.println("29219042977");
+    }
+    
+}
